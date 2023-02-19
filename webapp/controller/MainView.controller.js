9e053a75fd21db76a788f34da0025d4fecc6e9bf
Richpearz <rperez@saljamex.com>
2023-02-19 18:11:55 +0000

    First Commit 04

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -6,10 +6,48 @@ sap.ui.define([
      */
     function (Controller) {
         "use strict";
+        /*
+        function myCheck()
+            var inputEmployee = this.byId("inputEmployee");
+            var valueEmployee = inputEmployee.getValue();
 
-        return Controller.extend("fioriapp.employees.controller.MainView", {
-            onInit: function () {
+            if(valueEmployee.length == 6){
+                //inputEmployee.setDescription("Ok");
+                this.byId("labelCountry").setVisible(true);
+                this.byId("slCountry").setVisible(true);
+            
+            }else{
+                //inputEmployee.setDescription("Not Ok");
+                this.byId("labelCountry").setVisible(false);
+                this.byId("slCountry").setVisible(false);
 
             }
-        });
+        }
+        */
+
+        var Main = Controller.extend("fioriapp.employees.controller.MainView", {});
+
+        Main.prototype.onInit = function () {
+            
+        }
+
+        Main.prototype.onValidate = function () {
+
+            var inputEmployee = this.byId("inputEmployee");
+            var valueEmployee = inputEmployee.getValue();
+
+            if(valueEmployee.length == 6){
+                //inputEmployee.setDescription("Ok");
+                this.getView().byId("labelCountry").setVisible(true);
+                this.getView().byId("slCountry").setVisible(true);
+            
+            }else{
+                //inputEmployee.setDescription("Not Ok");
+                this.getView().byId("labelCountry").setVisible(false);
+                this.getView().byId("slCountry").setVisible(false);
+
+            }
+        }
+
+        return Main;
     });
